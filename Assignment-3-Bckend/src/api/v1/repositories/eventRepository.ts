34d2109e896f9e66546ed80df79faf8cb94c26a5
jaspreet <jkaur126@academic.rrc.ca>
2026-04-24 09:31:16 -0500

    feat: add firestore event repository.

---
+++ Assignment-3-Bckend/src/api/v1/repositories/eventRepository.ts
@@ -0,0 +1,58 @@
+import { db } from "../../../../config/firebaseConfig";
+import { Event } from "../models/eventModel";
+
+const collection = db.collection("events");
+
+export const createEvent = async (event: Event) => {
+  const docRef = await collection.add(event);
+  return { id: docRef.id, ...event };
+};
+
+export const getAllEvents = async () => {
+  const snapshot = await collection.get();
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
+export const getEventById = async (id: string) => {
+  const doc = await collection.doc(id).get();
+
+  if (!doc.exists) {
+    return null;
+  }
+
+  return {
+    id: doc.id,
+    ...doc.data(),
+  };
+};
+
+export const updateEvent = async (id: string, event: Partial<Event>) => {
+  const docRef = collection.doc(id);
+  const doc = await docRef.get();
+
+  if (!doc.exists) {
+    return null;
+  }
+
+  await docRef.update(event);
+
+  return {
+    id,
+    ...event,
+  };
+};
+
+export const deleteEvent = async (id: string) => {
+  const docRef = collection.doc(id);
+  const doc = await docRef.get();
+
+  if (!doc.exists) {
+    return false;
+  }
+
+  await docRef.delete();
+  return true;
+};
